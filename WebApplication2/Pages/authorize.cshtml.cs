059694eafcf769df9513abf73d9ee5b1d3a0aed9
cilantrol <gaowhui@gmail.com>
2023-11-13 09:32:50 -0800

    Add project files.

---
+++ WebApplication2/Pages/authorize.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication2.Pages
+{
+    public class authorizeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
